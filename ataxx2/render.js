584d9404ac6d5ed19b1445403f8db9f0bdd37fe1
정상현 <96582264+J-SangHyun@users.noreply.github.com>
2023-05-25 08:09:14 +0900

    fdsgdsfg

--- ataxx2/render.js
+++ ataxx2/render.js
@@ -2,9 +2,25 @@ import { app } from './main.js';
 import { BOARD_SIZE, board, toggle, points } from './game.js';
 
 export function render_all() {
+  render_board_line();
 
 }
 
 function render_board_line() {
   
+}
+
+function render_pieces() {
+
+}
+
+function render_action() {
+
+}
+
+function render_toggle() {
+  if(toggle != undefined) {
+    (x, y) = toggle;
+    
+  }
 }
